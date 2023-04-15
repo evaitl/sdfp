4d75d86d2c946683d98360e9f54e88ae6479561b
evaitl <evaitl@gmail.com>
2023-04-15 11:00:41 -0400

    rewrite of static buf

--- lib/sdfp.c
+++ lib/sdfp.c
@@ -131,62 +131,36 @@ static int __init sdfp_init(void)
 module_init(sdfp_init);
 
 /**
-   Create a new node, or use the static node.
-
-   Static node is unused if it's start is 0. 
- */
-static struct sdfp_node *new_node(void *buf, uintptr_t start, uintptr_t end)
-{
-	struct sdfp_node *nn = current->sdfp_list;
-	if (nn->start) {
-		// Static node is in use, create a new node.
-		nn = kmalloc(sizeof(struct sdfp_node), GFP_KERNEL);
-		if (!nn) {
-			return 0;
-		}
-		nn->buf = kmalloc(end - start, GFP_KERNEL);
-		if (!nn->buf) {
-			kfree(nn);
-			return 0;
-		}
-	}
-	nn->start = start;
-	nn->end = end;
-	memcpy(&nn->buf[0], buf, end - start);
-	return nn;
-}
-/**
-   data_check() - Check a new sdfp_node for multi-reads
-   @nn: the node to check
+   @buf: the buf to check
+   @start: user space start addr
+   @end: user space end address
 
    See if there are any overlaps with something on
-   &current->sdfp_list. If so, overwrite @nn->buf with original data.
-
-   nn may be in static node, don't trip on that.
+   &current->sdfp_list. If so, overwrite buf with original data. 
    
    RETURNS:
-   %true if there was a mult-read.
+   %true if there was a modified multi-read.
  */
-static bool data_check(struct sdfp_node *nn)
+static bool data_check(uint8_t *buf, uintptr_t start, uintptr_t end)
 {
 	const int nr = current->sdfp_nr;
 	struct sdfp_node *cn = current->sdfp_list;
 	bool ret = false;
 	while (cn) {
-		if ((cn != nn) && (cn->start < nn->end) &&
-		    (cn->end > nn->start)) {
+		if ((cn->start < end) &&
+		    (cn->end > start)) {
 			// There is an overlap.
-			uintptr_t ostart = max(cn->start, nn->start);
-			uintptr_t oend = min(cn->end, nn->end);
+			uintptr_t ostart = max(cn->start, start);
+			uintptr_t oend = min(cn->end, end);
 			atomic_inc(&num_multis[nr]);
 			if (!test_and_set_bit(nr, sdfp_multiread_reported))
 				printk(KERN_ALERT
 				       "SDFP: Multi-read detected in pid %d syscall %d",
 				       current->pid, nr);
 			if (memcmp(&cn->buf[ostart - cn->start],
-				   &nn->buf[ostart - nn->start],
+				   &buf[ostart - start],
 				   oend - ostart)) {
-				memcpy(&nn->buf[ostart - nn->start],
+				memcpy(&buf[ostart - start],
 				       &cn->buf[ostart - cn->start],
 				       oend - ostart);
 				ret = true;
@@ -198,15 +172,32 @@ static bool data_check(struct sdfp_node *nn)
 }
 
 /**
-   Add a new node to the `current->sdfp_list` after the static node. 
+   If the static node isn't in use, place the data there, else
+   create a new node and link it in after the static node. 
  */
-static void add_node(struct sdfp_node *nn)
+static void add_node(uint8_t *buf, uintptr_t start, uintptr_t end)
 {
 	struct sdfp_node *cn = current->sdfp_list;
-	if (nn != cn) {
-		nn->next = cn->next;
-		cn->next = nn;
-	}
+        if(cn->start){
+                // Start buffer is used. Create a new buf and link it in after
+                // start buffer.
+                struct sdfp_node *nn=kmalloc(sizeof(struct sdfp_node), GFP_KERNEL);
+                if (!nn){
+                        printk(KERN_ALERT "SDFP: malloc failure");
+                        return;
+                }
+                nn->buf=kmalloc(end-start, GFP_KERNEL);
+                if (!nn->buf){
+                        printk(KERN_ALERT "SDFP: malloc failure");
+                        kfree(nn);
+                        return;
+                }
+                nn->next=cn->next;
+                cn=nn;
+        }
+        cn->start=start;
+        cn->end=end;
+        memcpy(cn->buf,buf,end-start);
 }
 
 /**
@@ -234,7 +225,6 @@ void sdfp_check(volatile void *to, const void __user *from, unsigned long n)
 	int nr = current->sdfp_nr;
 	uintptr_t start = (uintptr_t)from;
 	uintptr_t end = start + n;
-	struct sdfp_node *nn = 0;
 	struct mutex *lock = &current->sdfp_lock;
 	if (!n || sdfp_no_check || pagefault_disabled())
 		return;
@@ -247,24 +237,16 @@ void sdfp_check(volatile void *to, const void __user *from, unsigned long n)
 		return;
 	atomic64_add(n, &num_bytes[nr]);
 	mutex_lock(lock);
-	nn = new_node((void *)to, start, end);
-	if (!nn) {
-		printk(KERN_ALERT "SDFP: Malloc failure in new node\n");
-		mutex_unlock(lock);
-		sdfp_clear(current, nr);
-		return;
-	}
-	if (data_check(nn)) {
+	if (data_check((uint8_t *)to,start,end)) {
 		printk(KERN_ALERT
 		       "SDFP: Modifed multi detected in pid %d syscall %d bytes %ld",
 		       current->pid, nr, n);
-		memcpy((void *)to, nn->buf, n);
 		if (sdfp_kill_doublefetch) {
 			printk(KERN_ALERT "SDFP: Killing pid %d", current->pid);
 			force_sig(SIGKILL);
 		}
 	}
-	add_node(nn);
+	add_node((uint8_t *)to, start, end);
 	mutex_unlock(lock);
 }
 EXPORT_SYMBOL(sdfp_check);
@@ -272,24 +254,25 @@ EXPORT_SYMBOL(sdfp_check);
 /**
    Clear the sdfp_list from task `tsk`. Called at the beginning of a
    syscall (on current) or when a task_struct is being cleaned up.
+
+   Setting the static node start field to 0 signals that it is not being used.
  */
 void sdfp_clear(struct task_struct *tsk, int nr)
 {
 	struct mutex *lock = &tsk->sdfp_lock;
-	struct sdfp_node *cn = 0;
+	struct sdfp_node *cn = tsk->sdfp_list;
 	mutex_lock(lock);
-	cn = tsk->sdfp_list;
 	cn->start = 0;
 	cn->end = 0;
 	cn = cn->next;
 	tsk->sdfp_list->next = 0;
-	mutex_unlock(lock);
 	while (cn) {
 		struct sdfp_node *nn = cn->next;
 		kfree(cn->buf);
 		kfree(cn);
 		cn = nn;
 	}
+	mutex_unlock(lock);
 	tsk->sdfp_nr = nr;
 }
 EXPORT_SYMBOL(sdfp_clear);
